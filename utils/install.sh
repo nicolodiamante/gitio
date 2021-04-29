bda23bde95e591fd636d65a416e02929b0859094
Nicolò Diamante <hello@nicolodiamante.com>
2021-04-29 18:18:07 +0200

    first commit

---
+++ utils/install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+#
+# Install gitio
+#
+
+# Determines the current user's shell, if `zsh` then installs.
+[[ "$(basename -- "$SHELL")" == "zsh" ]] || exit 1
+
+# Defines the PATHs.
+SCRIPT="${HOME}/gitio/script"
+ZSHRC="${ZDOTDIR:-${XDG_CONFIG_HOME/zsh:-$HOME}}/.zshrc"
+
+if [[ -d "$SCRIPT" && -f "$ZSHRC" ]]; then
+  # Appends to `zshrc`.
+  cat << EOF >> ${ZSHRC}
+# gitio
+fpath=(~/gitio/script \$fpath)
+autoload -Uz gitio
+EOF
+  echo 'zsh: appended gitio to zshrc.'
+
+  # Reloads shell.
+  source "${ZSHRC}"
+else
+  echo 'zsh: zshrc not found!'
+fi
